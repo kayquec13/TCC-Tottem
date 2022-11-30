b7d3300de92b406e262617a3ec53d90d2c117192
Kayque Benatti <kayque.benatti@sinqia.com.br>
2022-11-29 22:02:15 -0300

    fix: change url axios create

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/'
+    ? 'http://10.0.2.2:8080/'
     : '',
   headers: {
     'Accept': 'application/json',
